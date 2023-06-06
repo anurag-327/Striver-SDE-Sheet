35cc6c694daa26ddec04daad13dc5a4cc276529b
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-06-06 19:10:14 +0530

    Maximum Subarray Sum

---
+++ Maximum-Subarray-Sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+   
+    long long maxi=INT_MIN;
+    long long sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        if(sum<0)
+        sum=0;
+        maxi=max(maxi,sum);
+    }
+    return maxi;
+}
