b1130346db9efd1b90660ee065aa67194993278d
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-06-07 23:42:28 +0530

    pascal triangle

---
+++ Pascal-Triangle.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+vector<vector<long long int>> printPascal(int n) 
+{
+  // Write your code here.
+  // starting and last element will be always 1
+  // arr[row][col]=arr[row-1][col-1]+arr[row-1][col]
+    vector<vector<long long int>> printPascal;
+    for(int row=0;row<n;row++)
+    {
+      vector<long long int> temp;
+      for(int col=0;col<=row;col++)
+      {
+         if(col==0 || col==row)
+            temp.push_back(1);
+         else
+            temp.push_back(printPascal[row-1][col-1]+printPascal[row-1][col]);   
+      }
+      printPascal.push_back(temp);
+    }
+    return printPascal;
+}
